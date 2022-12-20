78253634231cacbd64e1b3f33251a52559169aaa
PrangonGhose <prangonghose@gmail.com>
2022-12-21 01:13:16 +0600

    Add leader object instance

---
+++ src/leaderObject.js
@@ -0,0 +1,8 @@
+// make object instance
+export default class Leader {
+  constructor(leaderName, leaderScore, index) {
+    this.leaderName = leaderName;
+    this.leaderScore = leaderScore;
+    this.index = index;
+  }
+}
