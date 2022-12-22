24a1cf7565e41ea3ca0a6f2fbdba8b21adee6726
PrangonGhose <prangonghose@gmail.com>
2022-12-23 01:07:28 +0600

    Fix linters

--- src/postGame.js
+++ src/postGame.js
@@ -2,22 +2,22 @@
 // This module is added to the repository only for learning purpose.
 // Running it again will only create a new game with a new unique ID.
 
-// The unique ID for the game is nbZrh2raj2305opaXN7n. This ID is used in the URL for posting and getting scores.
+// The unique ID for the game is nbZrh2raj2305opaXN7n.
+// This ID is used in the URL for posting and getting scores.
 
-const postData = () => {
-  const url = "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/";
-  const data = {"name": "Game of thrones"};
+/* const postData = () => {
+  const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+  const data = { name: 'Game of thrones' };
   const params = {
     method: 'post',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify(data)
-  }
-  fetch(url, params).then(response => response.json()).then((data) => {
-    const gameID = data;
-    console.log(gameID);
-  })
-}
+    body: JSON.stringify(data),
+  };
+  fetch(url, params).then((response) => response.json()).then((data) => {
+    console.log(data);
+  });
+};
 
-postData();
+postData(); */
