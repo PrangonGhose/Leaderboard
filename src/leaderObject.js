e99f07215eefa480dfd08f3240b11924bd587eb9
PrangonGhose <prangonghose@gmail.com>
2022-12-23 00:53:45 +0600

    Remove leaderObject.js

--- src/leaderObject.js
+++
@@ -1,8 +0,0 @@
-// make object instance
-export default class Leader {
-  constructor(leaderName, leaderScore, index) {
-    this.leaderName = leaderName;
-    this.leaderScore = leaderScore;
-    this.index = index;
-  }
-}
