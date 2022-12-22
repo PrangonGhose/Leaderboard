403680d28fe91803c970317b347d7afa004d3e77
PrangonGhose <prangonghose@gmail.com>
2022-12-23 01:07:08 +0600

    Fix linters

--- src/formAdd.js
+++ src/formAdd.js
@@ -1,16 +1,16 @@
 // Import modules
-import postScore from "./postScore.js";
-import UI from "./domObject.js";
+import postScore from './postScore.js';
+import UI from './domObject.js';
 
 // Grab necessary elements from HTML
 const leaderName = document.querySelector('.leader-name');
 const leaderScore = document.querySelector('.leader-score');
 
-const uiObject = new UI; // declare new DOM object
+const uiObject = new UI(); // declare new DOM object
 
 const addToForm = async () => {
   await postScore(leaderName.value, leaderScore.value); // wait until input is recorded
   uiObject.clearInput();
-}
+};
 
-export {addToForm, uiObject};
+export { addToForm, uiObject };
